35b947fdbeca3b403bf69bae4dc250bf5c7af097
mayaSun <maya166@gmail.com>
2015-11-24 20:35:03 +0200

    Try to fix bug regarding production

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module AvrahamProject
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
+    config.i18n.locale = :en
     config.assets.precompile += ['blueprint/screen.css', 'jquery.timepicker.css']
   end
 end
