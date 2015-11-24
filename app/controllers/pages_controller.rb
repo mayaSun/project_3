a3ff3fd32633b4105f3ec7f64a432c01dcf1366e
mayaSun <maya166@gmail.com>
2015-11-25 01:29:15 +0200

    Try to fix bug regarding production

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,11 +16,11 @@ class PagesController <ApplicationController
   end
 
   def set_language
+    Chapter.create(book: I18n.locale)
     I18n.locale = params[:locale].to_sym 
-    #respond_to do |format|
-      #format.js {render inline: "location.reload();" }
-    #end
-    redirect_to root_path
+    respond_to do |format|
+      format.js {render inline: "location.reload();" }
+    end
   end
 
 end
