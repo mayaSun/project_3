cc0d37c91ee4cd6880e2db30dc8e838714747fc8
mayaSun <maya166@gmail.com>
2015-11-25 00:57:39 +0200

    Try to fix bug regarding production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  #before_action :default_url_options
+  before_action :default_url_options
   helper_method :set_admin , :is_admin?
 
   def set_admin
