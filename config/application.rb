da882d29d54881e45822046219fe9fffdd03988c
mayaSun <maya166@gmail.com>
2015-11-24 20:28:23 +0200

    Try to fix bug regarding production

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,8 @@ module AvrahamProject
     config.active_record.raise_in_transactional_callbacks = true
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    #config.i18n.default_locale = :ar
+    #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :en
     config.assets.precompile += ['blueprint/screen.css', 'jquery.timepicker.css']
   end
 end
